5df6379616de93a8657c8549723518104525a197
victrhugo <victorhugonego@gmail.com>
2025-08-01 16:33:46 -0300

    fix: authcontroller

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,16 +10,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final UserService userService;
-
-    public AuthController(UserService userService) {
-        this.userService = userService;
-    }
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<?> register (@RequestBody User user) {
-        if (userService.emailJaExiste(user.getEmail()));
-        return ResponseEntity.badRequest().body("E-mail já existente.");
+    public ResponseEntity<?> register(@RequestBody User user) {
+        if (userService.emailJaExiste(user.getEmail())) {
+            return ResponseEntity.badRequest().body("E-mail já existente.");
+        }
+
+        User novoUsuario = userService.salvar(user);
+        return ResponseEntity.ok("Usuário cadastrado com sucesso: " + novoUsuario.getEmail());
     }
-    
 }
